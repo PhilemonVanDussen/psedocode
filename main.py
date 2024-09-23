86aff0a712a603902fce9eced63c89239e2f8963
PhilemonVanDussen <vandussenph709@northwested.org>
2024-09-23 14:21:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# PJ VanDussen
+# 9/23/2024
+# Psedocode Right-Hand Triangle Calculation
 
+#Input
+width = float(input('Enter the width of the triangle: \n'))
+height = float(input('Enter the height of the triangle: \n'))
+
+#Process
+area = width * height / 2
+
+#Output
+print(f'The area of the triangle is '+ str(area))
